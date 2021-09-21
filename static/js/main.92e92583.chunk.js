bce72e236dc74b040b1cf5b011ddd9d8bfe8b89e
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-21 20:56:59 +0800

    Updates

--- static/js/main.92e92583.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-demo1"]=this["webpackJsonpreact-demo1"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),o=n(3),s=n.n(o),a=(n(8),n(9),n(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:"Say Hello world!"})};s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.92e92583.chunk.js.map
